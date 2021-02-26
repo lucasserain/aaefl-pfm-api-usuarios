726abb006deade10a43e5ef16aa9cac50dfdc7dc
Lucas Serain <lucas.serain@outlook.com>
2021-02-25 21:33:55 -0300

    :wrench: Configurações de ESlint e prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Joooj' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello Joooj' }),
+);
 
 export default routes;
