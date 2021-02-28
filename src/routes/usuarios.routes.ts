172a94aadd4901ab5bf96f400f7e58023c0bdac6
Lucas Serain <lucas.serain@outlook.com>
2021-02-27 22:35:17 -0300

    :card_file_box: Migration - adição de campo de avatar

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CriarUsuarioService from '../services/CriarUsuarioService';
 
+import garantirAutenticacao from '../middlewares/garantirAutenticacao';
+
 const usuariosRouter = Router();
+const upload = multer(uploadConfig);
 
 usuariosRouter.post('/', async (request, response) => {
   try {
@@ -24,5 +29,14 @@ usuariosRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usuariosRouter.patch(
+  '/avatar',
+  garantirAutenticacao,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: 'jooj' });
+  },
+);
 
 export default usuariosRouter;
